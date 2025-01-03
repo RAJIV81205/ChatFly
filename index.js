4b1f5e5b87444f1d7510035bccd0eac6d45b773c
Rajiv <lucky81205@gmail.com>
2025-01-03 17:33:11 +0530

    new

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require ('express');
 const mongoose = require ('mongoose');
 const cors = require ('cors');
-const path = require ('path');
-PORT = 3000
+const dotenv = require ('dotenv');
+const nodemon = require ('nodemon');
 
 
 const app = express();
 app.use(cors);
+dotenv.config();
+PORT = process.env.PORT;
+URI = process.env.MONGODB_URI;
 
-app.use(express.static(path.join(__dirname, 'public')))
 
 
+mongoose.connect(URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
 
 
 
