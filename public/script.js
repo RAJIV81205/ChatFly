9de694c2a96c957412a0efc66b432b64a2c36b5a
Rajiv <lucky81205@gmail.com>
2025-01-04 22:38:21 +0530

    be

--- public/script.js
+++ public/script.js
@@ -2,3 +2,38 @@ document.addEventListener('DOMContentLoaded' , ()=>{
     const loader = document.querySelector('.blob-container')
     loader.style.display = "none"
 })
+
+try {
+    document.getElementById('dashboard-button').addEventListener('click',()=>{
+        token = localStorage.getItem('token');
+        const status = verifyToken(token)
+        if(status === "Valid Token"){
+            window.location.href = 'dashboard.html';
+        }
+        else{
+            alert("Invalid or Expired Token")
+            window.location.href = "login.html"
+        }
+    })
+} catch (error) {
+    
+}
+
+
+async function verifyToken(token){
+    const response = await fetch ("http://localhost:3000/verify-token",{
+        headers:{
+            'Content-Type': 'application/json',
+        },
+        body:JSON.stringify({token})
+
+    })
+    const data = await response.json()
+    if (response.ok){
+        console.log(data)
+    }
+    else{
+        console.error("Error Verifying Token:",error)
+    }
+
+}
