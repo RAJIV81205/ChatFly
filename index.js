671f7bddc2dfa5153ce393c2d2c1e206d2f8b803
Rajiv <lucky81205@gmail.com>
2025-01-16 20:24:32 +0530

    .

--- index.js
+++ index.js
@@ -227,6 +227,10 @@ io.on('connection', (socket) => {
         }
     });
 
+    socket.on("connect_error", (err) => {
+        console.log(`connect_error due to ${err.message}`);
+      });
+
 
 });
 
