801b30f3b43943f59190554d5f2acb7da923423d
Rajiv <lucky81205@gmail.com>
2025-05-18 16:07:57 +0530

    Enhance Dashboard component by adding ChatSidebar and Chats components for improved layout and functionality

--- components/Dashboard/Dashboard.tsx
+++ components/Dashboard/Dashboard.tsx
@@ -1,8 +1,20 @@
+"use client"
+
+
 import React from 'react'
+import ChatSidebar from './ChatSidebar'
+import Chats from './Chats'
 
 const Dashboard = () => {
   return (
-    <div className='h-screen w-full grid grid-cols-10'></div>
+    <div className='h-screen w-full grid grid-cols-10'>
+        <div className='col-span-2 border-r border-gray-300'>
+            <ChatSidebar />
+        </div>
+        <div className='col-span-8'>
+            <Chats />
+        </div>
+    </div>
   )
 }
 
