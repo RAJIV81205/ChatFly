3639c7a5ef4ad7e18197e25b0cd7eb5f1c2c1ae7
Rajiv <lucky81205@gmail.com>
2025-05-18 15:51:36 +0530

    Add DashboardPage component to render the Dashboard

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import Dashboard from "@/components/Dashboard/Dashboard";
+
+import React from 'react'
+
+const DashboardPage = () => {
+  return (
+    <Dashboard />
+  )
+}
+
+export default DashboardPage
---
+++ components/Dashboard/Dashboard.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Dashboard = () => {
+  return (
+    <div className='h-screen w-full grid grid-cols-10'></div>
+  )
+}
+
+export default Dashboard
